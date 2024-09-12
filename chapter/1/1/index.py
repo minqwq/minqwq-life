5a626e027174da6568c4ff0e58090201822916a2
minqwq <minqwq@github.com>
2024-09-12 08:58:42 +0800

    Redesigned

---
+++ chapter/1/1/index.py
@@ -0,0 +1,18 @@
+# Chapter 1-1 by minqwq
+import pygame
+import os
+import sys
+pygame.mixer.init()
+pygame.mixer.music.load("./music/False_Strawberry_Memories_remix__--_minqwq.mp3")
+pygame.mixer.music.play()
+print("幻想乡，某座山上 | 2017-04-08")
+input("") # 这个东西表示"按下enter键显示下一句" | minqwq | 2024-09-12
+print("<???> 抱歉----麻烦能打扰一下吗？")
+input("")
+print("我听到一个可爱的声音叫住了我。")
+input("")
+print("<???> 厕...厕所在哪里？")
+input("")
+print("<minqwq> 这条路前面有个分岔路口，你左转应该就能看到了。")
+input("")
+print("<???> (快速离开)")
---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import colorama as color
+import os
+import sys
+import pygame
+randomExitText = ["<Yukari> 再见...希望我们还能再见。", "<Melted> 再见", "<minqwq> by", "<白九> 晚安", "<Rick Astley> Say goodbye", "<mcpe> 没事，再见"]
+pygame.mixer.init()
+pygame.mixer.music.load("./music/Final_Fight_--_minqwq_usedformsl.mp3")
+pygame.mixer.music.play()
+print("minqwq's life | minqwq 的一生 | (v0.1uncon) by minqwq 和他的几个朋友")
+print("[" + color.Fore.LIGHTRED_EX + "!" + color.Fore.LIGHTWHITE_EX + "] 极早期开发阶段，不代表正式后效果。")
+print("请选择选项...")
+print("\n1:新的一生")
+print("2:选择特定章节(不会做保存功能)")
+print("3:睡觉(退出)")
+space = 0
+while True:
+    selectButton = input("> ")
+    if selectButton == "1":
+        pygame.mixer.music.stop()
+        os.system("python3 ./chapter/1/1/index.py")
+    elif selectButton == "2":
+        print("目前只有一个没有子章节的大章，先不做")
+    elif selectButton == "3":
+        random.shuffle(randomExitText)
+        randomExitTextPrinted = randomExitText[0]
+        print(randomExitTextPrinted)
+        break
+        sys.exit()
