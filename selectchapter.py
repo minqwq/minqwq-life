6977ece517eea4a999f53854ebed87196b5ee66f
minqwq <minqwq@github.com>
2024-09-17 14:59:14 +0800

    lost file added

---
+++ selectchapter.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import pygame
+pygame.mixer.init()
+print("选择章节...")
+selectedChapter = input("请按此格式输入章节:大章/小章\n示例:1/2\n> ")
+os.system("python3 ./chapter/" + selectedChapter + "/index.py")
+
