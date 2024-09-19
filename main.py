e4606cede45a1ab492d5c6b2dacc733845670488
minqwq <minqwq@github.com>
2024-09-19 09:35:24 +0800

    Some update but its not 0.2 release its unfinished.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ randomExitText = ["<Yukari> 再见...希望我们还能再见。", "<Melted> 再
 pygame.mixer.init()
 pygame.mixer.music.load("./music/Final_Fight_--_minqwq_usedformsl.mp3")
 pygame.mixer.music.play()
-print("minqwq's life | minqwq 的一生 | (v0.1uncon) by minqwq 和他的几个朋友")
-print("[" + color.Fore.LIGHTRED_EX + "!" + color.Fore.LIGHTWHITE_EX + "] 极早期开发阶段，不代表正式后效果。")
+print("minqwq's life | minqwq 的一生 | (v0.2) by minqwq 和他的几个朋友")
+print("[" + color.Fore.LIGHTRED_EX + "!" + color.Fore.LIGHTWHITE_EX + "] 早期开发阶段，不代表正式后效果。")
 print("请选择选项...")
 print("\n1:新的一生")
 print("2:选择特定章节(不会做保存功能)")
@@ -20,8 +20,8 @@ while True:
         pygame.mixer.music.stop()
         os.system("python3 ./chapter/1/1/index.py")
     elif selectButton == "2":
-        os.system("python3 selectchapter.py")
         pygame.mixer.music.stop()
+        os.system("python3 selectchapter.py")
     elif selectButton == "3":
         random.shuffle(randomExitText)
         randomExitTextPrinted = randomExitText[0]
