47b9893a5143d6fbaea17895e8809557e4fe5cd5
minqwq <minqwq723897@outlook.com>
2025-01-14 23:06:22 +0800

    sync to debian

--- selectchapter.py
+++ selectchapter.py
@@ -4,5 +4,5 @@ import pygame
 pygame.mixer.init()
 print("选择章节...")
 selectedChapter = input("请按此格式输入章节:大章/小章\n示例:1/2\n> ")
-os.system("python3 ./chapter/" + selectedChapter + "/index.py")
+os.system("python ./chapter/" + selectedChapter + "/index.py")
 
